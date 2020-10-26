bb6a8c1855036961891fec206f4cb8186b1b76db
YS-McGee <metalonly40@gmail.com>
2020-10-26 16:01:43 +0800

    Changed UI.

--- app/src/main/java/com/example/phonedialer/CallReceiver.java
+++ app/src/main/java/com/example/phonedialer/CallReceiver.java
@@ -5,18 +5,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.widget.Toast;
 
 public class CallReceiver  extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
-            Log.d("call", "Call Started...");
+            Log.d("callstate", "Call Started...");
+            Toast.makeText(context, "Call Started...", Toast.LENGTH_LONG).show();
         }
         else if (intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_IDLE)) {
-            Log.d("call", "Call ended...");
+            Log.d("callstate", "Call ended...");
+            Toast.makeText(context, "Call Ended...", Toast.LENGTH_LONG).show();
         }
         else if (intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_RINGING)) {
-            Log.d("call", "Incoming call..");
+            Log.d("callstate", "Incoming call..");
+            Toast.makeText(context, "Incoming Call...", Toast.LENGTH_LONG).show();
+            // callStateTextView.setText("Call State  Incoming Call...");
         }
     }
 }
